2581948c3a2ecdb48983e3793277ef57ba4c0f7a
Gonzalo <gvilugron@live.cl>
2025-08-01 12:34:18 +0100

    feat: add redirect from root URL to /api/books/

--- bookcatalog/urls.py
+++ bookcatalog/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/api/books/", permanent=False)),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
