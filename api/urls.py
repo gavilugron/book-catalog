17763c3a5d0842722fd7091fc107a0bf2571991a
Gonzalo <gvilugron@live.cl>
2025-08-01 13:33:10 +0100

    fix: add app_name to api URLs and set namespace for URL inclusion

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
+app_name = 'api'
+
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
 
